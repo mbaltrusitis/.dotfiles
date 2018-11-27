fd5a1f5ccc9621390517d7c02dd72434400e39f5
mbaltrusitis <matthew@baltrusitis.com>
2018-11-27 07:06:16 -0500

    Adds base backup scripts

---
+++ bin/backup/base-backup.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+
+function cleanup() {
+	echo "I: Cleaning up"
+	unset DEST
+	unset GPG_KEY
+	unset OLDEST_FULL_TIME
+	unset PASSPHRASE
+	unset RETENTION_TIME
+	unset SIGN_PASSPHRASE
+	unset SRC
+}
+
+
+echo "I: Starting backup."
+if hash duplicity 2>/dev/null; then
+	if duplicity \
+		--verbosity=info \
+		--asynchronous-upload \
+		--encrypt-sign-key="$GPG_KEY" \
+		--exclude-if-present=.nobackup \
+		--full-if-older-than="$OLDEST_FULL_TIME" \
+		"$SRC" \
+		"$DEST"; then
+		echo "I: B2 Backup complete";
+		echo "I: Verifying B2 backup";
+	else
+		echo "E: Backup error"
+		cleanup
+		exit 1;
+	fi;
+
+	if duplicity verify \
+		--verbosity=info \
+		--encrypt-sign-key="$GPG_KEY" \
+		--exclude-if-present=.nobackup \
+		"$DEST" \
+		"$SRC"; then
+		echo "I: Verify complete."
+	else
+		echo "E: Could not verify B2 backup."
+		cleanup
+		exit 1;
+	fi
+
+
+	echo "I: Truncating backups"
+	if duplicity remove-older-than "$RETENTION_TIME" \
+		--force \
+		"$DEST"; then
+		echo "I: Truncate complete";
+	else
+		echo "E: Truncate error"
+		cleanup
+		exit 1;
+	fi;
+
+	echo "I: Cleaning up backend for non-duplicity nonsense"
+	if duplicity cleanup \
+		--force \
+		"$DEST"; then
+		echo "I: Cleanup successful"
+	else
+		echo "E: Cleanup error"
+		exit 1;
+	fi;
+
+	cleanup
+	echo "I: 🎉Done🎉"
+	exit 0;
+else
+	echo "E: Duplicity is not installed"
+	cleanup
+	exit 1
+fi;
+
+echo "E: Unknown error"
+cleanup
+exit 1;
