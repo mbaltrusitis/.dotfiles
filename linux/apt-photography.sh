1f126fe1517266d45300237c47dabb4e19ce3585
Matthew Baltrusitis <matthew@baltrusitis.com>
2019-07-01 12:39:29 -0400

    Adds photo workflow dep script

---
+++ linux/apt-photography.sh
@@ -0,0 +1,18 @@
+# color management
+gnome-color-manager
+dispcalgui
+# camera tethering
+entangle
+# photo imports
+rapid-photo-downloader
+libmediainfo0v5
+# RAW editing
+add-apt-repository ppa:pmjdebruijn/darktable-release
+	darktable
+raw-therapee
+# pixel editing
+gimp
+add-apt-repository ppa:otto-kesselgulasch/gimp
+	gmic gimp-gmic
+# panorama
+hugin
