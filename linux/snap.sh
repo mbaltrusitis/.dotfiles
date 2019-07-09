89116ca5cf3546eade449171e1a8d62551c00a27
Matthew Baltrusitis <matthew@baltrusitis.com>
2019-07-08 22:16:20 -0400

    Cleans up snap script

--- linux/snap.sh
+++ linux/snap.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-sudo snap refresh
-sudo snap install --classic slack
-sudo snap install dbeaver-ce
-sudo snap install mailspring
-sudo snap install signal-messenger
+echo "I: Installing snap-based images";
+sudo snap refresh;
+sudo snap install --classic slack;
+sudo snap install dbeaver-ce;
+sudo snap install mailspring;
+sudo snap install signal-messenger;
+sudo snap install --classic google-cloud-sdk;
