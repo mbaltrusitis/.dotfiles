a68d6923b6dbd5867d23c01b5cb12bbdc3c3b7a4
Matthew Baltrusitis <matthew@baltrusitis.com>
2019-08-12 15:24:55 -0400

    Updates bash_history ignore

--- bash_backup.sh
+++ bash_backup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-TARGET_DIR="${1-$HOME}/.bash*"
+TARGET_DIR="${1-$HOME}"
+TARGET_FILES="$TARGET_DIR/.bash*"
 
 echo "Moving or unlinking Bash-related files in $TARGET_DIR"
 echo ""
 
-for file in $TARGET_DIR; do
-	if [ "$file" == "**/.bash_history" ]; then
+for file in $TARGET_FILES; do
+	if [ "$file" == "$TARGET_DIR/.bash_history" ]; then
 		echo "Skipping .bash_history";
 	elif [ -h "$file" ]; then
 		echo "Unlinking symbolic link: $file"
