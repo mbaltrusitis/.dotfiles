8e33e89e95214465396dbfcf5361d73b2da03293
Matthew Baltrusitis <matthew@baltrusitis.io>
2023-11-15 17:50:48 -0500

    Adds video to gif script

---
+++ bin/.local/bin/gifencode.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+palette="/tmp/palette.png"
+filters="fps=24,scale=800:-1:flags=lanczos"
+
+ffmpeg -v warning -i "$1" -vf "$filters,palettegen" -y "$palette"
+ffmpeg -v warning -i "$1" -i "$palette" -lavfi "$filters [x]; [x][1:v] paletteuse" -y "$2"
+
