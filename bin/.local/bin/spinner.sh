3888bd9a3def1e8f298ab6e317e527cfb245282e
Matthew Baltrusitis <matthew@baltrusitis.io>
2024-10-19 13:48:48 -0400

    refactor: clean-up spinner code and hide cursor

--- bin/.local/bin/spinner.sh
+++ bin/.local/bin/spinner.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # @file spin.sh
-# @example: spin.sh rsync -a ./local/dir host@remote:///some/dir
+# @example: spin.sh sleep 3
 #
 # @description Add a spinner to any long running `<command>`
 # @arg $1 string Long running command the spinner should wait for
 #
 # spinner characters
-readonly spin_str='⬝ ⬞ ▫ ◻ 󰝣 ◻ ▫ ⬞ ⬝'
 
 # @description Loops spinner until `pid` exits
 # @arg $1 A pid for a long running command
 spin() {
+  trap 'tput cnorm; exit' INT TERM # Ensure cursor is reset on exit
+  readonly spin_str='⬝ ⬞ ▫ ◻ 󰝣 ◻ ▫ ⬞ ⬝'
   local pid="$1"
   local delay=0.1
   local spin_chars=($spin_str)
+
   local i=0
 
   while kill -0 "$pid" 2>/dev/null; do
@@ -25,4 +27,6 @@ spin() {
 }
 
 exec "$@" &
+tput civis # hide the cursor
 spin $!
+tput cnorm # show the cursor
